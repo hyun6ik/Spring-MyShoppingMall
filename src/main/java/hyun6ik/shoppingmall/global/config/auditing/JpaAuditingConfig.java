240d79e2d84f50bd4378c2b7bb2860b1f7f7157d
hyun6ik <fkanwm@naver.com>
2022-07-19 00:45:07 +0900

    feat(#45) : ElasticSearch 설정

--- src/main/java/hyun6ik/shoppingmall/global/config/auditing/JpaAuditingConfig.java
+++ src/main/java/hyun6ik/shoppingmall/global/config/auditing/JpaAuditingConfig.java
@@ -1,13 +1,19 @@
 package hyun6ik.shoppingmall.global.config.auditing;
 
-import lombok.RequiredArgsConstructor;
+import hyun6ik.shoppingmall.infrastructure.delivery.repository.DeliveryRepository;
+import hyun6ik.shoppingmall.infrastructure.item.repository.ItemImageRepository;
+import hyun6ik.shoppingmall.infrastructure.item.repository.ItemRepository;
+import hyun6ik.shoppingmall.infrastructure.member.repository.MemberRepository;
+import hyun6ik.shoppingmall.infrastructure.order.repository.OrderRepository;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
-@Configuration
+@EnableJpaRepositories(basePackageClasses = {DeliveryRepository.class, MemberRepository.class, ItemRepository.class,
+        ItemImageRepository.class, OrderRepository.class})
 @EnableJpaAuditing
-@RequiredArgsConstructor
+@Configuration
 public class JpaAuditingConfig {
 
 }
