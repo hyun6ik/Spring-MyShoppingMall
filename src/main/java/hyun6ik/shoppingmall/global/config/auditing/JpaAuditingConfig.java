0c81f5cb9e7f8a3df14b08cd1e8ada8912944d36
hyun6ik <fkanwm@naver.com>
2022-07-21 19:53:17 +0900

    feat(#53) : JpaConfig Profile 설정

--- src/main/java/hyun6ik/shoppingmall/global/config/auditing/JpaAuditingConfig.java
+++ src/main/java/hyun6ik/shoppingmall/global/config/auditing/JpaAuditingConfig.java
@@ -6,10 +6,12 @@ import hyun6ik.shoppingmall.infrastructure.item.repository.ItemRepository;
 import hyun6ik.shoppingmall.infrastructure.member.repository.MemberRepository;
 import hyun6ik.shoppingmall.infrastructure.order.repository.OrderRepository;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
+@Profile({"local","dev","prod"})
 @EnableJpaRepositories(basePackageClasses = {DeliveryRepository.class, MemberRepository.class, ItemRepository.class,
         ItemImageRepository.class, OrderRepository.class})
 @EnableJpaAuditing
