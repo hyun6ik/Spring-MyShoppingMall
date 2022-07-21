73d2e1211551dd3a12d0be8081b5794769e84854
hyun6ik <fkanwm@naver.com>
2022-07-21 21:09:21 +0900

    refactor(#58) : test 설정

--- src/main/java/hyun6ik/shoppingmall/global/config/auditing/JpaAuditingConfig.java
+++ src/main/java/hyun6ik/shoppingmall/global/config/auditing/JpaAuditingConfig.java
@@ -6,7 +6,6 @@ import hyun6ik.shoppingmall.infrastructure.item.repository.ItemRepository;
 import hyun6ik.shoppingmall.infrastructure.member.repository.MemberRepository;
 import hyun6ik.shoppingmall.infrastructure.order.repository.OrderRepository;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
