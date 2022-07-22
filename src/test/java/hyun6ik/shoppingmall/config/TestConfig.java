bf289618619df90c103bd95acad973434f167e0c
hyun6ik <fkanwm@naver.com>
2022-07-22 16:02:01 +0900

    refactor(#58) : test 설정

---
+++ src/test/java/hyun6ik/shoppingmall/config/TestConfig.java
@@ -0,0 +1,38 @@
+package hyun6ik.shoppingmall.config;
+
+import hyun6ik.shoppingmall.infrastructure.delivery.repository.DeliveryRepository;
+import hyun6ik.shoppingmall.infrastructure.item.elasticsearch.ItemEsRepository;
+import hyun6ik.shoppingmall.infrastructure.item.repository.ItemImageRepository;
+import hyun6ik.shoppingmall.infrastructure.item.repository.ItemRepository;
+import hyun6ik.shoppingmall.infrastructure.member.repository.MemberRepository;
+import hyun6ik.shoppingmall.infrastructure.order.repository.OrderRepository;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableElasticsearchRepositories(basePackageClasses = ItemEsRepository.class)
+@EnableJpaRepositories(basePackageClasses = {DeliveryRepository.class, MemberRepository.class, ItemRepository.class,
+        ItemImageRepository.class, OrderRepository.class})
+@EnableJpaAuditing
+@TestConfiguration
+public class TestConfig extends AbstractElasticsearchConfiguration {
+
+    @Value("${elasticsearch.host}")
+    private String host;
+    @Value("${elasticsearch.port}")
+    private Integer port;
+
+    @Override
+    public RestHighLevelClient elasticsearchClient() {
+        ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+                .connectedTo(host + ":" + port)
+                .build();
+        return RestClients.create(clientConfiguration).rest();
+    }
+}
