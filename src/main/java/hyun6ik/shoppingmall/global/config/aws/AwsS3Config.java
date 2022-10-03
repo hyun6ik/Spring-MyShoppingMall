dd79f714d575da7cbc769e7a344fd7a897ffdbec
hyun6ik <fkanwm@naver.com>
2022-10-04 01:23:39 +0900

    refactor(#93) : AWS Properties 삭제

--- src/main/java/hyun6ik/shoppingmall/global/config/aws/AwsS3Config.java
+++ src/main/java/hyun6ik/shoppingmall/global/config/aws/AwsS3Config.java
@@ -6,17 +6,23 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Slf4j
-@EnableConfigurationProperties(AwsCredentialsProperties.class)
 @Configuration
 @RequiredArgsConstructor
 public class AwsS3Config {
 
-    private final AwsCredentialsProperties awsCredentialsProperties;
+    @Value("${cloud.aws.credentials.access-key")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
 
     static {
         System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
@@ -24,9 +30,9 @@ public class AwsS3Config {
 
     @Bean
     public AmazonS3Client amazonS3Client() {
-        BasicAWSCredentials awsCreds = new BasicAWSCredentials(awsCredentialsProperties.getAccessKey(), awsCredentialsProperties.getSecretKey());
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
         return (AmazonS3Client) AmazonS3ClientBuilder.standard()
-                .withRegion("ap-northeast-2")
+                .withRegion(region)
                 .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
                 .build();
     }
