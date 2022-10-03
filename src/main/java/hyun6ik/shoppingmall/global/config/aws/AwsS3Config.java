b108284caf4848ad7c4c578a16a7d3f81305dd7e
hyun6ik <fkanwm@naver.com>
2022-10-04 01:02:47 +0900

    refactor(#92) : AWS Properties 생성

--- src/main/java/hyun6ik/shoppingmall/global/config/aws/AwsS3Config.java
+++ src/main/java/hyun6ik/shoppingmall/global/config/aws/AwsS3Config.java
@@ -6,7 +6,6 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,9 +18,6 @@ public class AwsS3Config {
 
     private final AwsCredentialsProperties awsCredentialsProperties;
 
-    @Value("${cloud.aws.region.static}")
-    private String region;
-
     static {
         System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
     }
@@ -30,7 +26,7 @@ public class AwsS3Config {
     public AmazonS3Client amazonS3Client() {
         BasicAWSCredentials awsCreds = new BasicAWSCredentials(awsCredentialsProperties.getAccessKey(), awsCredentialsProperties.getSecretKey());
         return (AmazonS3Client) AmazonS3ClientBuilder.standard()
-                .withRegion(region)
+                .withRegion("ap-northeast-2")
                 .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
                 .build();
     }
