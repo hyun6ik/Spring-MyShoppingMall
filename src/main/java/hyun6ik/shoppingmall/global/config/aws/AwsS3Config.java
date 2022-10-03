8d7ffe3981e6c336b49135bbdb4a1b4df86a8e29
hyun6ik <fkanwm@naver.com>
2022-10-04 00:48:45 +0900

    refactor(#92) : AWS Properties 생성

--- src/main/java/hyun6ik/shoppingmall/global/config/aws/AwsS3Config.java
+++ src/main/java/hyun6ik/shoppingmall/global/config/aws/AwsS3Config.java
@@ -4,20 +4,20 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Slf4j
+@EnableConfigurationProperties(AwsCredentialsProperties.class)
 @Configuration
+@RequiredArgsConstructor
 public class AwsS3Config {
 
-    @Value("${cloud.aws.credentials.access-key}")
-    private String accessKey;
-
-    @Value("${cloud.aws.credentials.secret-key}")
-    private String secretKey;
+    private final AwsCredentialsProperties awsCredentialsProperties;
 
     @Value("${cloud.aws.region.static}")
     private String region;
@@ -28,7 +28,7 @@ public class AwsS3Config {
 
     @Bean
     public AmazonS3Client amazonS3Client() {
-        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials(awsCredentialsProperties.getAccessKey(), awsCredentialsProperties.getSecretKey());
         return (AmazonS3Client) AmazonS3ClientBuilder.standard()
                 .withRegion(region)
                 .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
