6424f87a17b9a4af9700c37f0b2edfa66d68f930
hyun6ik <fkanwm@naver.com>
2022-10-10 02:12:59 +0900

    feat(#126) : webserver - userdata.sh certbot 자동 설치 추가

--- terraform/ec2-instance/webserver/templates/userdata.sh
+++ terraform/ec2-instance/webserver/templates/userdata.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+## Nginx
 sudo apt update
 
 sudo apt install nginx -y
 
 sudo service start nginx
+
+## Certbot
+sudo snap install core
+sudo snap install --classic certbot
+
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
