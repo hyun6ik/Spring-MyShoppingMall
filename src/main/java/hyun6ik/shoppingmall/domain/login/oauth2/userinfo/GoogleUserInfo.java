c96a637e7af40dabb31995f472f8b93369573dd5
hyun6ik <fkanwm@naver.com>
2022-10-23 16:20:27 +0900

    refactor(#146) : 구글 로그인 확장성 있게 리펙토링

--- src/main/java/hyun6ik/shoppingmall/domain/login/oauth2/userinfo/GoogleUserInfo.java
+++ src/main/java/hyun6ik/shoppingmall/domain/login/oauth2/userinfo/GoogleUserInfo.java
@@ -1,19 +1,17 @@
 package hyun6ik.shoppingmall.domain.login.oauth2.userinfo;
 
 import hyun6ik.shoppingmall.domain.member.constant.MemberType;
-import lombok.Builder;
 
 import java.util.Map;
 
 public class GoogleUserInfo extends OAuth2Attributes {
 
-    @Builder
     public GoogleUserInfo(Map<String, Object> attributes, String nameAttributeKey, String memberName, String email, MemberType memberType) {
         super(attributes, nameAttributeKey, memberName, email, memberType);
     }
 
-    public static GoogleUserInfo of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
-        return GoogleUserInfo.builder()
+    public static OAuth2Attributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        return OAuth2Attributes.builder()
                 .memberName((String) attributes.get("name"))
                 .email((String) attributes.get("email"))
                 .attributes(attributes)
