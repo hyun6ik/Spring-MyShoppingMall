a414a91d23fa1db00d133c2d7381af4b8a500022
hyun6ik <fkanwm@naver.com>
2022-10-24 15:49:14 +0900

    fix(#158) : ElasticSearch - 검색 띄어쓰기 발생 시 나타나는 오류 수정

--- src/main/java/hyun6ik/shoppingmall/infrastructure/item/elasticsearch/ItemEsQueryRepository.java
+++ src/main/java/hyun6ik/shoppingmall/infrastructure/item/elasticsearch/ItemEsQueryRepository.java
@@ -7,22 +7,24 @@ import hyun6ik.shoppingmall.domain.item.entity.ItemDocument;
 import hyun6ik.shoppingmall.interfaces.main.dto.MainItemDto;
 import hyun6ik.shoppingmall.interfaces.main.dto.QMainItemDto;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.query.criteria.internal.compile.CriteriaQueryTypeQueryAdapter;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.SearchHit;
 import org.springframework.data.elasticsearch.core.SearchHits;
-import org.springframework.data.elasticsearch.core.query.Criteria;
-import org.springframework.data.elasticsearch.core.query.CriteriaQuery;
-import org.springframework.data.elasticsearch.core.query.Query;
+import org.springframework.data.elasticsearch.core.query.*;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.criteria.CriteriaBuilder;
 import java.util.List;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import static hyun6ik.shoppingmall.domain.item.entity.QItem.*;
 import static hyun6ik.shoppingmall.domain.item.entity.QItemImage.*;
+import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
 
 @Repository
 @RequiredArgsConstructor
@@ -32,8 +34,9 @@ public class ItemEsQueryRepository {
     private final JPAQueryFactory queryFactory;
 
     public Page<MainItemDto> searchMainItemsBy(String searchQuery, Pageable pageable) {
-        final Criteria criteria = Criteria.where("itemName").contains(searchQuery)
-                .or("itemDetail").contains(searchQuery);
+        final Criteria criteria = Criteria.where("itemName").expression("*" + searchQuery + "*")
+                .or("itemDetail").expression("*" + searchQuery + "*");
+
         final Query query = new CriteriaQuery(criteria);
         final SearchHits<ItemDocument> search = elasticsearchOperations.search(query, ItemDocument.class);
         final List<Long> ids = search.stream()
